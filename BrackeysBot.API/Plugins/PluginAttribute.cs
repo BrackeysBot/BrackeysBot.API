e5fc28baf92148080d3149c4ac8958203ddb548e
Oliver Booth <me@olivr.me>
2022-04-08 11:32:51 +0100

    Remove version parameter for PluginAttribute
    Semantic version can be retrieved from assembly attributes

--- BrackeysBot.API/Plugins/PluginAttribute.cs
+++ BrackeysBot.API/Plugins/PluginAttribute.cs
@@ -12,11 +12,9 @@ public sealed class PluginAttribute : Attribute
     ///     Initializes a new instance of the <see cref="PluginAttribute" /> class.
     /// </summary>
     /// <param name="name">The name of this plugin.</param>
-    /// <param name="version">The version of this plugin.</param>
-    public PluginAttribute(string name, string version)
+    public PluginAttribute(string name)
     {
         Name = name ?? throw new ArgumentNullException(nameof(name));
-        Version = version ?? throw new ArgumentNullException(nameof(version));
     }
 
     /// <summary>
@@ -24,10 +22,4 @@ public sealed class PluginAttribute : Attribute
     /// </summary>
     /// <value>The name of the plugin.</value>
     public string Name { get; }
-
-    /// <summary>
-    ///     Gets the version of this plugin.
-    /// </summary>
-    /// <value>The version of this plugin.</value>
-    public string Version { get; }
 }
