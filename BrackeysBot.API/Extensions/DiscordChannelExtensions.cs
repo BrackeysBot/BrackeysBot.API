4405382082c82be4599d7154f8b4e0fa9e0a49a5
Oliver Booth <me@olivr.me>
2022-04-18 10:39:51 +0100

    Add DiscordChannel.GetCategory extension method

--- BrackeysBot.API/Extensions/DiscordChannelExtensions.cs
+++ BrackeysBot.API/Extensions/DiscordChannelExtensions.cs
@@ -11,6 +11,24 @@ namespace BrackeysBot.API.Extensions;
 public static class DiscordChannelExtensions
 {
     /// <summary>
+    ///     Gets the category of this channel.
+    /// </summary>
+    /// <param name="channel">The channel whose category to retrieve.</param>
+    /// <returns>The category of this channel, or <see langword="null" /> if this channel is not defined in a category.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="channel" /> is <see langword="null" />.</exception>
+    public static DiscordChannel? GetCategory(this DiscordChannel channel)
+    {
+        if (channel is null) throw new ArgumentNullException(nameof(channel));
+
+        while (true)
+        {
+            if (channel.IsCategory) return channel;
+            if (channel.Parent is not { } parent) return null;
+            channel = parent;
+        }
+    }
+
+    /// <summary>
     ///     Normalizes a <see cref="DiscordChannel" /> so that the internal client is assured to be a specified value.
     /// </summary>
     /// <param name="channel">The <see cref="DiscordChannel" /> to normalize.</param>
